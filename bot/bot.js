7d445265996cc871e8066eeb5b8bf4487f48ca00
Marlon Card <marloncard@gmail.com>
2020-01-09 13:37:37 -0500

    Add bot

---
+++ bot/bot.js
@@ -0,0 +1,54 @@
+/* ----------------------------*/
+// 'Please Commit' - Discord Bot
+/* ----------------------------*/
+
+const fs = require('fs'); // Import File System Module
+const path = require('path'); //Import Path Module
+const homedir = require('os').homedir(); // Define home dir using OS Module
+
+const fpath = path.join(homedir, '.credentials/DISCORD.json');
+
+// Synchronous call to read file 
+const jsonData = fs.readFileSync(fpath);
+let auth = JSON.parse(jsonData);
+
+/* ----------------------------*/
+
+const Discord = require('discord.io');
+const logger = require('winston');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+const bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
